1c4cad92a443de7ef718a907c1d3689db105c883
Eduard Mititiuc <edikutsu2002@mail.ru>
2023-07-27 09:44:32 +0300

    aaa

--- Endava-Internship/webapp/src/main/java/com/example/demo/repository/DepartmentDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentDAO extends JpaRepository<Department,Integer> {
-}
--- Endava-Internship/webapp/src/main/java/com/example/demo/repository/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeDAO extends JpaRepository<Employee,Integer> {
-    Employee getByEmail(String email);
-    Employee getByPhoneNumber(long phoneNumber);
-}
